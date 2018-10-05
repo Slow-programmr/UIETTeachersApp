8ef66373475f240e67fb6992d2c225f3915d0a2c
Slow-programmr <surjit9464@gmail.com>
2018-10-05 00:46:20 +0000

    Project Files Added

---
+++ app/src/main/java/com/example/surjit/uietteacherapp/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.surjit.uietteacherapp;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
